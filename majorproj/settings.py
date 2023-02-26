e3afc8ffcb0ef71e50ba4d598f45084bb387f2b1
keerthi <nimmagaddakeerthi18@gmail.com>
2023-02-26 19:35:11 +0530

    Register,Login completed

--- majorproj/settings.py
+++ majorproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mbti.apps.MbtiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'majorproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MBTI',
+        'USER' : 'postgres',
+        'PASSWORD' : 'mbti',
+        'HOST' : 'localhost'
     }
 }
 
